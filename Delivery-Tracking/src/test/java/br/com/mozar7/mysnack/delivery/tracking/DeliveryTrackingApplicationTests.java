c88d32bf5d9fc3f1d944c6990a48a4954b5a4cf7
AdsonMozart <mozar7.dev@gmail.com>
2025-07-26 00:29:04 -0300

    chore: add .gitignore and create first services, courier management and delivery tracking

---
+++ Delivery-Tracking/src/test/java/br/com/mozar7/mysnack/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mozar7.mysnack.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
