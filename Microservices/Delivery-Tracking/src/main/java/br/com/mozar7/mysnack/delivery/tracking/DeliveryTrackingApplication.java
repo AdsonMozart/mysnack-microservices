11116988429ad19b8936fdc3194a429770fe9b3a
AdsonMozart <mozar7.dev@gmail.com>
2025-07-28 00:13:34 -0300

    chore: already configs

--- Microservices/Delivery-Tracking/src/main/java/br/com/mozar7/mysnack/delivery/tracking/DeliveryTrackingApplication.java
+++ Microservices/Delivery-Tracking/src/main/java/br/com/mozar7/mysnack/delivery/tracking/DeliveryTrackingApplication.java
@@ -2,6 +2,7 @@ package br.com.mozar7.mysnack.delivery.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class DeliveryTrackingApplication {
